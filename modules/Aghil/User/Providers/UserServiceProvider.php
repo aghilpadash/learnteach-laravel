afdbcca20a199743a399556c56044d4f313318aa
Aghil Padash <aghilpadash73@gmail.com>
2021-11-08 21:46:36 +0330

    update providers

--- modules/Aghil/User/Providers/UserServiceProvider.php
+++ modules/Aghil/User/Providers/UserServiceProvider.php
@@ -8,19 +8,25 @@ namespace Aghil\User\Providers;
 
 use Aghil\User\Models\User;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/user_routes.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Factories');
+        $this->loadViewsFrom(__DIR__ . '/../Resources/Views', 'User');
+
+        Factory::guessFactoryNamesUsing(function ($modelName) {
+            return 'Aghil\User\Database\Factories\\' . class_basename($modelName) . 'Factory';
+        });
         config()->set('auth.providers.users.model', User::class);
     }
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/user_routes.php');
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Factories');
-        $this->loadViewsFrom(__DIR__ . '/../Resources/Views', 'User');
+
     }
 }
